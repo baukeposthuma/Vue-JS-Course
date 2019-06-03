edffde7918c7a2fc437b18ee2afb8964c4877d3f
Bauke Posthuma <baukeposthuma@gmail.com>
2019-06-03 14:17:07 +0200

    Add all exercises  for section 14 - 17

---
+++ vuex-state-management/src/components/Result.vue
@@ -0,0 +1,19 @@
+<template>
+    <p>Counter is: {{ counter }}</p>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex';
+    export default {
+        // computed: {
+        //     counter() {
+        //         return this.$store.getters.doubleCounter;
+        //     }
+        // }
+        computed: {
+            ...mapGetters([
+                'doubleCounter',
+            ])
+        }
+    }
+</script>
